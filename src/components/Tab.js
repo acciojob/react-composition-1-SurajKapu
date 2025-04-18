1b5fb4f6fefeecc04c6f84737da2ef3bf7ca2d56
Suraj Kapu <suraj@acciojob.com>
2025-04-18 20:10:59 +0530

    minor changes

---
+++ src/components/Tab.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const Tab = ({ tabData }) => {
+  const [content, setContent] = useState(tabData[0].content);
+
+  return (
+    <div>
+      <ul>
+        {tabData.map((tab, index) => (
+          <li key={index} onClick={() => setContent(tab.content)}>
+            {tab.title}
+          </li>
+        ))}
+      </ul>
+      <p>{content}</p>
+    </div>
+  );
+};
+
+export default Tab;
